29456e904fa09a83873b354f008519e2fc4e5cb8
Lava <69821200+RealLava@users.noreply.github.com>
2023-07-24 16:09:30 -0400

    Update change-logo.sh

--- change-logo.sh
+++ change-logo.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+set +x
+echo "Link of the logo you want to change it to (.svg)"
+read logo_link
+cd /var/www/pterodactyl/public/assets/svgs
+rm "pterodactyl.svg"
+curl -o "pterodactyl.svg" "$logo_link"
